3c6edd791d5ffa79afc3f320f7d4baf8d00cd36a
cukminseo <kms39273@catholic.ac.kr>
2024-11-13 17:50:33 +0900

    feat/temperature 및 token 답변길이, 프롬프트 조정

--- eda/get_gpt-answer.py
+++ eda/get_gpt-answer.py
@@ -9,10 +9,10 @@ from loguru import logger
 logger.add("gpt_answers_log.log", rotation="1 MB")  # 1MB 넘을 시 새 파일 생성
 
 # OpenAI API 클라이언트 초기화
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="")
 
 data = "../data/train.csv"
-model = "gpt-3.5-turbo"
+model = "gpt-4o"
 
 # CSV 파일 읽기
 df = pd.read_csv(data)
@@ -33,13 +33,16 @@ def generate_answer(paragraph, question, choices):
     {choices_text}
 
     정답을 선택지 번호로만 답하시오.
-    단하나의 정답만 도출하시오.
+    단하나 정답의 번호만 반드시 도출하시오.
+    정답 번호 이외의 다른 숫자는 출력하지 마시오.
     """
 
     # OpenAI API를 사용하여 답변 생성
     response = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
         model=model,
+        temperature=0,  # 온도 설정
+        max_tokens=8,     # 응답 토큰 길이 제한
     )
 
     # 모델의 응답을 번호로 추출
@@ -56,7 +59,7 @@ for index, row in df.iterrows():
 
     try:
         answer, request_id = generate_answer(paragraph, question, choices)
-        answer_parsed = re.search(r'\b[1-4]\b', answer)  # 1에서 4까지의 숫자만 찾음
+        answer_parsed = re.search(r'\b[1-5]\b', answer)  # 1에서 4까지의 숫자만 찾음
 
         if answer_parsed:
             answer_parsed = answer_parsed.group()  # 매칭된 첫 번째 숫자 추출
@@ -73,6 +76,6 @@ for index, row in df.iterrows():
     time.sleep(1)
 
 # CSV 파일로 저장
-output_path = data.replace(".csv", f"_add={model}-answer.csv")
+output_path = data.replace(".csv", f"_add-{model}-answer.csv")
 df.to_csv(output_path, index=False)
 logger.info(f"Results saved to {output_path}")
